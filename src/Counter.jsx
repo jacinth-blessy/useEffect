07f939eadd2e2ceadb4c341258897b405362743e
jacinth-blessy <jacinthmblessy2024@gmail.com>
2025-12-18 22:03:38 +0530

    added code for counter component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 function Counter()
 {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        if(count%3==0 && count!==0){
+            alert(`The current number ${count} is divisible by 3!`);
+        }
+    }, [count]);
+
     return (
         <div>
             <p>Count: {count}</p>
